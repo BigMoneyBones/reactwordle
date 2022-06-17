1344a32a4737e87c03414c6c66ba2e9f2cd87002
BigMoneyBones <87784559+BigMoneyBones@users.noreply.github.com>
2022-06-16 21:07:39 -0500

    update thursday night

--- src/App.js
+++ src/App.js
@@ -1,123 +1,87 @@
-import { useState } from "react";
-import "./App.css";
-
-const defaultGuessList = [
-  ["R", "E", "A", "C", "T"],
-  ["", "", "", "", ""],
-  ["", "", "", "", ""],
-  ["", "", "", "", ""],
-  ["", "", "", "", ""],
-  ["", "", "", "", ""],
-];
-
-const defaultKeyboard = [
-  ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
-  ["A", "S", "D", "F", "G", "H", "J", "K", "L"],
-  ["DELETE", "Z", "X", "C", "V", "B", "N", "M", "ENTER"],
-];
-
+import React, { useEffect, useState } from "react";
+import Wordle from "./components/Wordle";
 function App() {
-  // Create variable for guess array, set intial value to 'defaultGuessList'
-  const [wordleGuessList, setWordleGuessList] = useState([...defaultGuessList]);
+  // state to store solution
+  const [solution, setSolution] = useState(null);
+  // useEffect takes in 2 arguments - a callback (a function) and a dependency array
+  useEffect(() => {
+    // Fetch 'solutions' from json file using JSON server
+    fetch("http://localhost:3001/solutions")
+      // Fetched json file needs to be passed to a usable javascript datastructure
+      // Take response and use .json method on it
+      .then((res) => res.json())
+      // Take json and print to console log to verify it is being grabbed successfully
+      .then((json) => {
+        // console.log(json);
+        // random int between 0 and length of solutions array
+        const randomSolution = json[Math.floor(Math.random() * json.length)];
+        // Do not want the entire object- just the word property of the object or the 'solution' word.
+        setSolution(randomSolution.word);
+      });
+    // Because we are using setSolution which is an external function, it has to be passed into the dependency array
+    // below as a dependency. The dependency array tells the callback function to trigger when the dependency array
+    // changes.
+  }, [setSolution]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        {/* call and display header element */}
-        <WordleHeader />
-        {/* Chain reaction, WordleGrid calls WordleGridRow which calls WordleGridLetter */}
-        <WordleGrid wordleGuessList={wordleGuessList} />
-        <KeyBoardComponent />
-      </header>
+      <h1>Wordle</h1>
+      {/* while solution is a false value (ie: initial value of 'null') react will not output the value on the right of the '&&'. *conditional template*
+      only output the Wordle component when we have a value for the 'solution' condition on the left of &&
+      pass solution in as a prop in the wordle component and assign it the value of solution */}
+      {/* {solution && <div>Solution is: {solution}</div>} */}
+      {solution && <Wordle solution={solution} />}
     </div>
   );
 }
 
-// 'Wordle Clone' header element component
-const WordleHeader = () => {
-  return <h1 className="wordle-header">Wordle Clone</h1>;
-};
-
-// WORDLE GUESS COMPONENTS
-
-const WordleGrid = (props) => {
-  // const { wordleGuessList } = props;
-  return (
-    <div className="wordle-grid">
-      {props.wordleGuessList.map((wordleGuess) => {
-        return <WordleGridRow wordleGuess={wordleGuess} />;
-      })}
-    </div>
-  );
-};
-
-const WordleGridRow = (props) => {
-  return (
-    <div className="wordle-grid-row">
-      {props.wordleGuess.map((wordleLetter) => {
-        return (
-          <WordleGridLetter
-            wordleLetter={wordleLetter}
-            isCorrect={wordleLetter === "A" || wordleLetter === "E"}
-          />
-        );
-      })}
-    </div>
-  );
-};
-
-const WordleGridLetter = (props) => {
-  return (
-    <div className={`wordle-grid-letter wordle-grid-letter-${props.isCorrect}`}>
-      {props.wordleLetter}
-    </div>
-  );
-};
-
-// KEYBOARD COMPONENTS
-
-//
-const KeyComponent = (props) => {
-  return <div className="Keyboard-key">{props.letter}</div>;
-};
-
-//
-const KeyRowComponent = (props) => {
-  return (
-    <div className="Keyboard-row">
-      {props.keyRow.map((letter, index) => {
-        return <KeyComponent key={index} letter={letter}></KeyComponent>;
-      })}
-    </div>
-  );
-};
-
-//
-const KeyBoardComponent = () => {
-  return (
-    <div className="Keyboard-grid">
-      {defaultKeyboard.map((row, index) => {
-        return <KeyRowComponent key={index} keyRow={row}></KeyRowComponent>;
-      })}
-    </div>
-  );
-};
-
 export default App;
 
-/*
-Use import/export syntax with react
-User require/module.export syntax with express
-// Default Export / Only exporting 1 thing
-export default App;
-import App from "./app.js"
-module.exports = App
-const App = require("./app.js")
----
-// Module Export / Exporting multiple things
-export App
-import { App } from "./app.js"
-module.exports = {
-  App: App
-}
-const { App } = require("./app.js") 
+/* 
+
+data we need to track:
+  -- solution
+    -- 5 letter string, e.g. 'drain'
+  -- past guesses
+    -- an array of past guesses
+    -- each past guess is an array of letter objects [{}, {}, {}, {}, {}]
+    -- each object represents a letter in the guess word {letter: 'a', color: 'yellow'}
+  -- current guess
+    -- string 'hello'
+  -- keypad letters
+    -- array of letter objects [{key: 'a', color: 'green'}, {}, {} ...]
+  -- number of turns
+    -- an integer 0 - 6
+
+game process:
+  -- entering words:
+    -- user enters a letter & a square is filled with that letter
+    -- when a user hits delete it deletes the previous letter
+    -- when a user hits enter it submits the word
+      -- if all squares are not filled with letters then the word is not submitted
+      -- if that word has already been used in a prev guess then the word is not submitted
+  -- checking submitted words:
+    -- each letter is checked to see if it matches to the solution
+    -- each letter is assigned a color based on it's inclusion in the solution
+      -- exact matches (correct position in the solution) are green
+      -- partial matches (in the solution but not the correct position) are yellow
+      -- none-matches (not in the solution at all) are grey
+    -- the guess is added the grid with the correct colors
+    -- the current guess moves to the next row
+    -- the keypad letters are updated (colors)
+  -- ending the game:
+    -- when the guessed word fully matches the solution
+      -- modal to say 'well done'
+    -- when the user runs out of guesses
+      -- modal to say 'unlucky'
+
 */
+
+// ***JSON SERVER & db.json file***
+
+// Pulling solution words from a json file.
+// Using JSON server to host our database (db.json) file
+// Installed json server using this command to install globally to the PC: npm install -g json-server
+// Run json server to wrap db.json file with API endpoints.
+// command: json-server ./data/db.json --port 3001
+// react is running on port 3000 so we assign db.json to port 3001
